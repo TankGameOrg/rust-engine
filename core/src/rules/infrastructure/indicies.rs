5f950dddf1a8098f3d48527c602dfef528af200d
Ryan Ray <ryan314r@gmail.com>
2024-11-10 01:10:42 +0000

    Add an index that can track attributes with multiple instances of the same value

--- core/src/rules/infrastructure/indicies.rs
+++ core/src/rules/infrastructure/indicies.rs
@@ -150,7 +150,7 @@ impl<T: HashableAttribute> Default for UniqueValueIndex<T> {
     }
 }
 
-impl<T: AttributeValue + Hash + std::cmp::Eq + Clone> Index for UniqueValueIndex<T> {
+impl<T: HashableAttribute> Index for UniqueValueIndex<T> {
     type AttributeValueType = T;
 
     fn add_attribute(
@@ -173,11 +173,107 @@ impl<T: AttributeValue + Hash + std::cmp::Eq + Clone> Index for UniqueValueIndex
     }
 }
 
+/// A wrapper for an Optional hash set iterator that behaves like a normal iterator
+struct OptionalIterator<'iter> {
+    parent: Option<std::collections::hash_set::Iter<'iter, Handle>>,
+}
+
+impl<'iter> Iterator for OptionalIterator<'iter> {
+    type Item = Handle;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(iter) = &mut self.parent {
+            iter.next().copied()
+        }
+        else {
+            None
+        }
+    }
+}
+
+/// An index for looking up entities by the value of an atribute when multiple entities will have the same value
+///
+/// ```
+/// # use tank_game_core::attribute;
+/// # use tank_game_core::rules::infrastructure::ecs::{Universe, Handle};
+/// # use tank_game_core::rules::infrastructure::indicies::NonUniqueValueIndex;
+/// # use std::vec::Vec;
+/// # use tank_game_core::create_entity;
+/// attribute!(DummyNonUnique: u32, indexed by NonUniqueValueIndex<u32>);
+/// 
+/// let mut universe = Universe::new();
+/// 
+/// let _ = create_entity!(&mut universe, { DummyNonUnique = 1 })?;
+/// let _ = create_entity!(&mut universe, { DummyNonUnique = 1 });
+/// 
+/// // Find all entities with the DummyNonUnique value of 1
+/// let _one = universe.get_index(&DummyNonUnique).gather_by(&1);
+/// #
+/// # Ok::<(), Box<dyn std::error::Error>>(())
+/// ```
+pub struct NonUniqueValueIndex<T: HashableAttribute> {
+    by_value: HashMap<T, HashSet<Handle>>,
+}
+
+impl<T: HashableAttribute> NonUniqueValueIndex<T> {
+    pub fn new() -> NonUniqueValueIndex<T> {
+        NonUniqueValueIndex {
+            by_value: HashMap::new(),
+        }
+    }
+
+    /// Gather all handles where their attribute value is equal to search
+    pub fn gather_by<'iter>(&'iter self, search: &T) -> impl Iterator<Item = Handle> + 'iter {
+        OptionalIterator {
+            parent: self.by_value.get(search).map(|set| set.iter()),
+        }
+    }
+
+    /// Collect all of the handles with this index
+    pub fn gather<'iter>(&'iter self) -> impl Iterator<Item = Handle> + 'iter {
+        self.by_value.values().flat_map(|set| set.iter()).cloned()
+    }
+}
+
+impl<T: HashableAttribute> Default for NonUniqueValueIndex<T> {
+    fn default() -> Self {
+        NonUniqueValueIndex::new()
+    }
+}
+
+impl<T: HashableAttribute> Index for NonUniqueValueIndex<T> {
+    type AttributeValueType = T;
+
+    fn add_attribute(
+            &mut self,
+            handle: Handle,
+            new_value: &Self::AttributeValueType,
+        ) -> Result<(), Box<dyn std::error::Error>> {
+        if !self.by_value.contains_key(new_value) {
+            self.by_value.insert(new_value.clone(), HashSet::new());
+        }
+
+        self.by_value.get_mut(new_value).unwrap().insert(handle);
+        Ok(())
+    }
+
+    fn remove_attribute(&mut self, handle: Handle, old_value: &Self::AttributeValueType) -> Result<(), Box<dyn std::error::Error>> {
+        let set = self.by_value.get_mut(old_value).unwrap();
+        set.remove(&handle);
+
+        if set.is_empty() {
+            self.by_value.remove(old_value);
+        }
+
+        Ok(())
+    }
+}
+
 #[cfg(test)]
 mod test {
     use crate::{attribute, create_entity, rules::infrastructure::ecs::{Handle, Universe}};
 
-    use super::{HasAttribute, UniqueValueIndex};
+    use super::*;
 
     attribute!(DummyHas: u32, indexed by HasAttribute<u32>);
 
@@ -213,5 +309,29 @@ mod test {
 
         let err = create_entity!(&mut universe, { DummyUnique = 1 });
         assert!(err.is_err());
+    }
+
+    attribute!(DummyNonUnique: u32, indexed by NonUniqueValueIndex<u32>);
+
+    #[test]
+    fn test_non_unique_index() {
+        let mut universe = Universe::new();
+
+        let one = create_entity!(&mut universe, { DummyNonUnique = 1 }).unwrap();
+        let one_two = create_entity!(&mut universe, { DummyNonUnique = 1 }).unwrap();
+        let two = create_entity!(&mut universe, { DummyNonUnique = 2 }).unwrap();
+
+        let index: &NonUniqueValueIndex<u32> = universe.get_index(&DummyNonUnique);
+        let ones: HashSet<Handle> = index.gather_by(&1).collect();
+        assert!(ones.contains(&one));
+        assert!(ones.contains(&one_two));
+
+        let twos: HashSet<Handle> = index.gather_by(&2).collect();
+        assert!(twos.contains(&two));
+
+        let all: Vec<Handle> = index.gather().collect();
+        assert!(all.contains(&one));
+        assert!(all.contains(&one_two));
+        assert!(all.contains(&two));
     }
 }
